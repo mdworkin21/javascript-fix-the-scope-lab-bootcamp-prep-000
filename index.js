4153e08500a995a65264debe9735ba8d444a8fc4
Matthew Dworkin <mdworkin21@gmail.com>
2017-10-14 20:14:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function add2(n) {
 
 }
 
-var funkyFunction = function outerFunction(function) {
+var funkyFunction = function outerFunction(n) {
   return function innerFunction() {
     return "FUNKY!";
   }
