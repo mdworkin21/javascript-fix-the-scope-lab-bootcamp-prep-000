05ae06edfa19bedb06f3481667216b7c2d903e91
Matthew Dworkin <mdworkin21@gmail.com>
2017-10-14 20:07:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,10 +28,10 @@ var funkyFunction = function outerFunction() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction;
+var theFunk = funkyFunction();
 
 
-console.log(theFunk());
+console.log(funkyFunction());
 
 
 myAnimal();
